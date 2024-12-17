e94e17062bde538f12c61bf3371afb37478d3b87
catsgarden99 <1476125515@qq.com>
2024-12-17 15:58:08 +0800

    添加摸鱼日历

--- demo_group_reply_file.py
+++ demo_group_reply_file.py
@@ -17,20 +17,27 @@ class MyClient(botpy.Client):
         _log.info(f"robot 「{self.robot.name}」 on_ready!")
 
     async def on_group_at_message_create(self, message: GroupMessage):
-        file_url = ""  # 这里需要填写上传的资源Url
-        uploadMedia = await message._api.post_group_file(
-            group_openid=message.group_openid, 
-            file_type=1, # 文件类型要对应上，具体支持的类型见方法说明
-            url=file_url # 文件Url
-        )
+        file_url = "https://api.j4u.ink/v1/store/redirect/moyu/calendar/today.png"  # 这里需要填写上传的资源Url
 
-        # 资源上传后，会得到Media，用于发送消息
-        await message._api.post_group_message(
-            group_openid=message.group_openid,
-            msg_type=7,  # 7表示富媒体类型
-            msg_id=message.id, 
-            media=uploadMedia
-        )
+        try:
+            uploadMedia = await message._api.post_group_file(
+                group_openid=message.group_openid, 
+                file_type=1, # 文件类型要对应上，具体支持的类型见方法说明
+                url=file_url # 文件Url
+            )
+
+            # 资源上传后，会得到Media，用于发送消息
+            await message._api.post_group_message(
+                group_openid=message.group_openid,
+                msg_type=7,  # 7表示富媒体类型
+                msg_id=message.id, 
+                media=uploadMedia
+            )
+        except Exception as e:
+            print(f"Caught an exception: {e}")
+
+
+        print("finish\n")
 
 if __name__ == "__main__":
     # 通过预设置的类型，设置需要监听的事件通道
--- demo_group_reply_text.py
+++ demo_group_reply_text.py
@@ -11,8 +11,8 @@ import requests
 
 test_config = read(os.path.join(os.path.dirname(__file__), "config.yaml"))
 with open('groupmember_city.json', 'r') as mem_city_file:
-    member_city = json.load(mem_city_file)
-print(member_city)
+    member_city = dict(json.load(mem_city_file))
+# print(member_city)
 gaodekey = test_config["gaodekey"]
 
 _log = logging.get_logger()
@@ -23,7 +23,6 @@ class MyClient(botpy.Client):
         _log.info(f"robot 「{self.robot.name}」 on_ready!")
 
     async def on_group_at_message_create(self, message: GroupMessage):
-        # print(message.content)
         # 天气
         print(message.author.member_openid)
         user_openid = message.author.member_openid
@@ -31,13 +30,32 @@ class MyClient(botpy.Client):
         reply_str = ""
         
         if message.content.startswith(" /天气 "):
-            address = message.content.replace(" ", "")[3:]
-            # if member_city
-            addressCode = requests.get("https://restapi.amap.com/v3/config/district",{"keywords":address,"subdistrict":0,"key":gaodekey}).json()["districts"][0]["adcode"]
-            weather = requests.get("https://restapi.amap.com/v3/weather/weatherInfo",{"city":addressCode,"key":gaodekey,"extensions":"base"}).json()
-            temperature = weather["lives"][0]["temperature"]
 
-            reply_str = address + "目前温度为：" + temperature + "度"
+            address = message.content.replace(" ", "")[3:]
+
+            if address == "":
+                if user_openid not in member_city:
+                    reply_str = "请设置默认城市"
+                else:
+                    address = member_city[user_openid]
+
+            addressCode_json = requests.get("https://restapi.amap.com/v3/config/district",{"keywords":address,"subdistrict":0,"key":gaodekey}).json()["districts"]
+            if len(addressCode_json) == 0:
+                reply_str = "不认识这个城市捏"
+            else:
+                addressCode = addressCode_json[0]["adcode"]
+                weather = requests.get("https://restapi.amap.com/v3/weather/weatherInfo",{"city":addressCode,"key":gaodekey,"extensions":"base"}).json()
+                temperature = weather["lives"][0]["temperature"]
+                reply_str = address + "目前温度为：" + temperature + "度"
+
+            # 回复
+            messageResult = await message._api.post_group_message(
+                group_openid=message.group_openid,
+                msg_type=0, 
+                msg_id=message.id,
+                content=reply_str
+                )
+            _log.info(messageResult)
 
         if message.content.startswith(" /天气默认城市 "):
             # 截取地址
@@ -47,16 +65,40 @@ class MyClient(botpy.Client):
             with open('groupmember_city.json', 'w') as mem_city_file:
                 json.dump(member_city,mem_city_file)
             reply_str = "已添加天气默认城市"
+            # 回复
+            messageResult = await message._api.post_group_message(
+                group_openid=message.group_openid,
+                msg_type=0, 
+                msg_id=message.id,
+                content=reply_str
+                )
+            _log.info(messageResult)
             
+        if message.content.startswith(" /摸鱼日历 "):
+            file_url = "https://api.j4u.ink/v1/store/redirect/moyu/calendar/today.png"  # 这里需要填写上传的资源Url
 
-        messageResult = await message._api.post_group_message(
-            group_openid=message.group_openid,
-              msg_type=0, 
-              msg_id=message.id,
-            #   content=f"收到了消息：{message.content}" + weather_str
-              content=reply_str
-              )
-        _log.info(messageResult)
+            try:
+                uploadMedia = await message._api.post_group_file(
+                    group_openid=message.group_openid, 
+                    file_type=1, # 文件类型要对应上，具体支持的类型见方法说明
+                    url=file_url # 文件Url
+                )
+
+                await message._api.post_group_message(
+                    group_openid=message.group_openid,
+                    msg_type=7,  # 7表示富媒体类型
+                    msg_id=message.id, 
+                    media=uploadMedia
+                )
+            except Exception as e:
+                # print(f"图片下载失败！ Caught an exception: {e}")
+                messageResult = await message._api.post_group_message(
+                    group_openid=message.group_openid,
+                    msg_type=0, 
+                    msg_id=message.id,
+                    content="api又坏了拿不到图片啦"
+                    )
+                _log.info(messageResult)
 
 
 if __name__ == "__main__":
